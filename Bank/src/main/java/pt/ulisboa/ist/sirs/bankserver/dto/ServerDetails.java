801a99ab0eccc0468f2dabb7c53ed2a5d9401cfb
alexfaisca <alex.c.2002@gmail.com>
2024-04-23 14:03:25 +0100

    removed redundant bank layer

--- Bank/src/main/java/pt/ulisboa/ist/sirs/bankserver/dto/ServerDetails.java
+++
@@ -1,4 +0,0 @@
-package pt.ulisboa.ist.sirs.bankserver.dto;
-
-public record ServerDetails(String address, Integer port, String qualifier) {
-}
--- Bank/src/main/java/pt/ulisboa/ist/sirs/bankserver/grpc/crypto/AuthenticationClientCryptographicCore.java
+++
@@ -1,6 +0,0 @@
-package pt.ulisboa.ist.sirs.bankserver.grpc.crypto;
-
-import pt.ulisboa.ist.sirs.cryptology.Base;
-
-public class AuthenticationClientCryptographicCore implements Base.CryptographicCore {
-}
